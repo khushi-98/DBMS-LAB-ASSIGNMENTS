95ba43228ec89ca72d08492174ec81ba57afeadd
Khushi Bansal <102850725+khushi-98@users.noreply.github.com>
2024-04-04 06:26:40 +0530

    Add files via upload

---
+++ dbms_assign3.sql
@@ -0,0 +1,131 @@
+REM   Script: Assignment_3
+REM   Assignment3
+
+select sysdate from dual ;
+
+select chr(66) from dual ;
+
+select concat('Tha','Par') from dual;
+
+select instr('Thapar','p') from dual ;
+
+select instr('Thapar','p') from dual ;
+
+select instr('Thapar','p') from dual ;
+
+select length('Thapar') from dual ;
+
+select lpad('Thapar',4,'*') from dual ;
+
+select length('Thapar') from dual ;
+
+select lpad('Thapar',14,'*') from dual ;
+
+ select ltrim('Thapar','tha') from dual ;
+
+ select ltrim('Thapar','p') from dual ;
+
+ select ltrim('Thapar','par') from dual ;
+
+ select ltrim('Thapar','Tha') from dual ;
+
+ select rpad('Thapar',10,'*') from dual ;
+
+select rtrim('Thapar','par') from dual ;
+
+select substr('Thapar',2,5) from dual ;
+
+ select rpad('Thapar',10,'*') from dual ;
+
+select rtrim('Thapar','par') from dual ;
+
+select replace('Thapar','ap','dd') from dual ;
+
+select substr('Thapar',2,4) from dual ;
+
+select initcap('Thapar') from dual ;
+
+select lower('Thapar') from dual ;
+
+select upper('Thapar') from dual ;
+
+select translate('Thapar','a','b') from dual ;
+
+select abs(-12.4) from dual ;
+
+select abs(-12.4) from dual ;
+
+select ceil(12.5) from dual ;
+
+select cos(90) from dual ;
+
+select exp(2) from dual ;
+
+select floor(20.12) from dual ;
+
+select mod(3,2) from dual ;
+
+select power(2,4) from dual ;
+
+select round(2.462,2) from dual ;
+
+select floor(20.12) from dual ;
+
+select mod(3,2) from dual ;
+
+select power(2,4) from dual ;
+
+select round(2.452,2) from dual ;
+
+select floor(20.12) from dual ;
+
+select mod(3,2) from dual ;
+
+select power(2,4) from dual ;
+
+select round(2.462,2) from dual ;
+
+select sign(-1) from dual ;
+
+select sqrt(16) from dual ;
+
+select trunc(5,2) from dual ;
+
+select sqrt(16) from dual ;
+
+select trunc(5.543,2) from dual ;
+
+select sysdate from dual ;
+
+select exp(2) from dual ;
+
+select exp(2) from dual ;
+
+select exp(2) from dual ;
+
+select exp(2) from dual ;
+
+select exp(2) from dual ;
+
+select add_months('01-feb-2024',4) from dual ;
+
+select exp(2) from dual ;
+
+select add_months('01-feb-2024',4) from dual ;
+
+select last_day('01-march-2024')from dual ;
+
+select months_between('2-feb-2024','2-april-2024') from dual ;
+
+select months_between('2-feb-2024','2-april-2024') from dual ;
+
+select next_day('4-April-2024','Thursday') from dual ;
+
+select greatest(23,20) from dual ;
+
+select greatest(23,20) from dual ;
+
+select least(-3,7,0) from dual ;
+
+select to_char(sysdate,'hh:mi:ss') as current_time from dual ;
+
